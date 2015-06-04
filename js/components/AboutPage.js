cec1f11a862b84e8952dc65b1fbc61e7e0769360
jurgob <jurgo.boemo@gmail.com>
2015-06-04 19:02:38 +0100

    add routing

---
+++ js/components/AboutPage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+
+
+class AboutPage extends React.Component
+{
+  render()
+  {  
+    return (
+    	<div>
+    		<h1>About Page</h1>
+      	<div>This is a react seed</div>
+  . </div>
+    );
+  }
+
+
+
+};
+
+export default AboutPage;
--- js/main.js
+++ js/main.js
@@ -1,13 +1,58 @@
 import  React 			from 'react';
 import  ResultPage 	from './components/ResultPage';
+import  AboutPage 	from './components/AboutPage';
 import AppActions 	from './actions/AppActions';
+var Router = require('react-router');
+var { Route, DefaultRoute, RouteHandler, Link } = Router;
+
 
 
 AppActions.getAllResults();
 
+
+var NavMenu = React.createClass({
+	render: function(){
+		return (<nav></nav>
+
+
+		)
+	}
+
+});
+
+
+
+var App = React.createClass({
+
+  render: function () {
+    return (
+      <div className="App">
+        <div className="AppMain">
+          <RouteHandler/>
+        </div>
+      </div>
+    );
+  }
+});
+
+
+var routes = (
+  <Route handler={App}>
+    <DefaultRoute handler={ResultPage}/>
+		<Route  name="about" path="about" handler={AboutPage}/>
+  </Route>
+);
+
+
+
 if(typeof(window) !== 'undefined'){
-	React.render(
-    <ResultPage />,
-    document.getElementById('content')
-	);
+	Router.run(routes, function (Handler) {
+	  React.render(<Handler/>, document.getElementById('content'));
+	});
+
+
+	// React.render(
+  //   <ResultPage />,
+  //   document.getElementById('content')
+	// );
 }
